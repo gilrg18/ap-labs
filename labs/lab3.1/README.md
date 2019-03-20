6970b1b4f44b2c6bcb07aea8986d4f184f78b6e7
gilrg18 <gilberto_rogel_26@hotmail.com>
2019-03-19 19:10:07 -0600

    Add files via upload

---
+++ labs/lab3.1/README.md
@@ -0,0 +1,25 @@
+Lab 3.1 - File/Directory Monitor
+================================
+
+Write a program that logs all file creations, deletions, and renames under the directory named in its command-line argument.
+The program should monitor events in all of the subdirectories under the specified directory.
+To obtain a list of all of these subdirectories, you will need to make use of [nftw()](https://linux.die.net/man/3/nftw).
+When a new subdirectory is added under the tree or a directory is deleted, the set of monitored subdirectories should be updated accordingly.
+
+General Requirements and Considerations
+---------------------------------------
+- Use the logger that was done on [lab2.4](https://github.com/CodersSquad/ap-labs/tree/master/labs/lab2.4).
+- Use the `inotify` [API](http://man7.org/linux/man-pages/man7/inotify.7.html).
+- Use the `monitor.c` file for implementing the lab's general flow.
+- Use the `Makefile` for compilation.
+- Update `README.md` with the proper steps for building and running your code.
+- Don't forget to handle errors properly.
+- Coding best practices implementation will be also considered.
+
+ gcc -c logger.c -o logger.o
+
+ gcc -c monitor.c -o monitor.o
+
+ gcc logger.o monitor.o -o main.o
+
+ ./main.o .
---
+++ labs/lab3.1/logger.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <syslog.h>
+
+#define RESET       0
+#define BRIGHT      1
+#define DIM         2
+#define UNDERLINE   3
+#define BLINK       4
+#define REVERSE     7
+#define HIDDEN      8
+
+#define BLACK       0
+#define RED         1
+#define GREEN       2
+#define YELLOW      3
+#define BLUE        4
+#define MAGENTA     5
+#define CYAN        6
+#define WHITE       7
+
+int type = 1;
+
+int initLogger(char *logType){
+    if(strcmp(logType, "stdout") == 0){
+        type = 1;
+    } else if (strcmp(logType, "syslog") == 0){
+        type = 2;
+    } else if (strlen(logType) == 0){
+        logType = "stdout";
+        type = 1;
+    }
+    printf("Initializing logger on %s\n", logType);
+    return 0;
+}
+
+int infof(const char *format, ...) {
+    if (type == 1){
+        int result;
+        va_list args;
+        va_start(args, format);
+        result = vfprintf(stdout, format, args);
+        va_end(args);
+        return result;
+    } else if (type == 2){
+        openlog("logger", LOG_PID|LOG_CONS, LOG_USER);
+        syslog(LOG_INFO, format);
+        closelog();
+    }
+}
+
+int warnf(const char *format, ...) {
+    if(type == 1){
+        textcolor(BRIGHT, YELLOW);  
+        int result;
+        va_list args;
+        va_start(args, format);
+        fputs("Warning: ", stdout);
+        result = vfprintf(stdout, format, args);
+        va_end(args);
+        textcolor(RESET, WHITE);  
+        return result;
+    } else if (type == 2){
+        openlog("logger", LOG_PID|LOG_CONS, LOG_USER);
+        syslog(LOG_WARNING, format);
+        closelog();
+    }
+}
+
+int errorf(const char *format, ...) {
+    if(type == 1){
+        textcolor(BRIGHT, RED);  
+        int result;
+        va_list args;
+        va_start(args, format);
+        fputs("Error: ", stdout);
+        result = vfprintf(stdout, format, args);
+        va_end(args);
+        textcolor(RESET, WHITE);
+        return result;
+    } else if (type == 2){
+        openlog("logger", LOG_PID|LOG_CONS, LOG_USER);
+        syslog(LOG_ERR, format);
+        closelog();
+    } 
+}
+
+int panicf(const char *format, ...) {
+    if(type == 1){
+        textcolor(BRIGHT, CYAN);  
+        int result;
+        va_list args;
+        va_start(args, format);
+        fputs("Panic: ", stdout);
+        result = vfprintf(stdout, format, args);
+        va_end(args);
+        textcolor(BRIGHT, CYAN);  
+        return result;
+    } else if (type == 2){
+        openlog("logger", LOG_PID|LOG_CONS, LOG_USER);
+        syslog(LOG_INFO, format);
+        closelog();
+    }
+}
+
+void textcolor(int attr, int fg){   
+    
+    char command[13];
+
+    sprintf(command, "%c[%d;%dm", 0x1B, attr, fg + 30);
+    printf("%s", command);
+    
+}
